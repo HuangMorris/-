637b6cb1a37e98d66a878af895ec014bacbb45e1
Morris <813106@junyi.tw>
2020-12-14 18:57:32 +0800

    second

---
+++ 基礎認識HTML.py
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <metal charset="utf-8"></metal>#用中文表達
+    <title>黃詠涒Morris</title>'''標題'''
+</head>
+<body>
+    <h3>我的照片My Picture</h3>'''三號標題'''
+    <img src="Me.jpg"/>'''src代表屬性。'''
+    <hr/>
+    <h3>我的最愛</h3>  
+    <ul>'''ul代表列表'''
+        <li><a href="https://about.google/?fg=1&utm_source=google-&utm_medium=referral&utm_campaign=hp-header">Google</a></li>
+        '''以<a>和</a>標籤中包覆GOOGLE，製作超連結。
+        href為標籤的屬性，後面接網址，打在<a>中:<li><a href="網址">網站名稱</a></li>'''
+        <li>Facebook</li>
+        <li>Instagram</li>'''li代表項目'''
+    </ul>
+    <h3>MLB BaseBall</h3>個人興趣
+    <table border="1" width="400" cellpadding="5">'''<table></table>代表要建立一個表格'''
+        <tr>'''tr代表橫向的列，此為第一列'''
+            <td>Name</td>'''第一列中第一欄'''
+            <td>Win</td>
+            <td>Lose</td>
+        </tr>
+        <tr>
+            <td><b>New York <u>Yankees</u></b></td>
+            <td>10</td>
+            <td>6</td>
+        </tr>
+        <tr>
+            <td><u>Bosten Red Sox</u></td>
+            <td>8</td>
+            <td>8</td>
+        </tr>
+    </table>
+</body>
+</html>
